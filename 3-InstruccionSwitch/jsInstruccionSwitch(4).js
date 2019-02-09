785c67c7cdbd3b22a714f5397f13b5972e705bec
diego <diegodalairac@gmail.com>
2019-02-08 22:29:36 -0300

    clase 2. Ej 3.Switch

--- 3-InstruccionSwitch/jsInstruccionSwitch(4).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(4).js
@@ -1,11 +1,27 @@
-function mostrar()
-{
-//tomo la edad  
-var mesDelAño = document.getElementById('mes').value;
+function mostrar() {
+    //tomo la edad  
+    var mesDelAño = document.getElementById('mes').value;
 
-//alert (mesDelAño);
-	
-	
+    switch (mesDelAño) {
+        case "Febrero":
+            alert("tiene 28 días.");
+            break;
+
+        case "Enero":
+        case "Marzo":
+        case "Mayo":
+        case "Julio":
+        case "Septiembre":
+        case "Noviembre":
+        case "Diciembre":
+            alert("tiene 31 días.");
+            break;
+
+        default:
+            alert(" tiene 30 días.");
+            break;
+
+    }
 
 
 
