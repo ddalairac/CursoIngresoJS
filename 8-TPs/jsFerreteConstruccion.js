ad36a8217ad20230412f1c55345c0961da06d738
diego <stereotipoarg@gmail.com>
2019-02-09 11:16:47 -0300

    TPs: facturacion y construccion

--- 8-TPs/jsFerreteConstruccion.js
+++ 8-TPs/jsFerreteConstruccion.js
@@ -6,13 +6,65 @@ C.	Para hacer un contrapiso de 1m x 1m se necesitan 2 bolsas de cemento y 3 de c
 
 function Rectangulo () 
 {
+	var largoTerreno;
+	var anchoTerreno;
+	var perimetro;
+	var cantidadCable;
+	var totalCable;	
+
+
+	largoTerreno = Largo.value;
+	anchoTerreno = Ancho.value;
+
+	largoTerreno = parseInt(largoTerreno); 
+	anchoTerreno = parseInt(anchoTerreno);
+
+	cantidadCable = 3;
+	perimetro = largoTerreno*2+anchoTerreno*2;
+	totalCable = perimetro * cantidadCable;
+
+	alert("Total de cable: "+totalCable);	
 
 }
 function Circulo () 
 {
+	var radioTerreno;
+	var cantidadCable;
+	var perimetro;
+	var totalCable;	
+
+	radioTerreno = Radio.value;
+	radioTerreno = parseInt(radioTerreno);
 	
+	cantidadCable = 3;
+	perimetro = Math.PI * radioTerreno * 2;
+	totalCable = perimetro * cantidadCable;
+	totalCable = totalCable.toFixed(2);
+
+	alert("Total de cable: "+totalCable);	
 }
 function Materiales () 
 {
+	var largoTerreno;
+	var anchoTerreno;
+	var superficie;
+	var cementoPormetro;
+	var calPormetro;
+	var totalCemento;
+	var totalCal;	
+
+	largoTerreno = Largo.value;
+	anchoTerreno = Ancho.value;
+
+	largoTerreno = parseInt(largoTerreno); 
+	anchoTerreno = parseInt(anchoTerreno);
+
+	cementoPormetro = 2;
+	calPormetro = 3;
+	superficie = largoTerreno * anchoTerreno;
+	totalCemento = superficie * cementoPormetro;
+	totalCal = superficie * calPormetro;
+
+	alert("Total de cemento: "+totalCemento+" - Total de cal: "+totalCal);	
 	
 }
--- 8-TPs/jsFerreteFacturacion.js
+++ 8-TPs/jsFerreteFacturacion.js
@@ -5,13 +5,64 @@ C.	ingresar tres precios de productos  y mostrar precio final (más IVA 21%).
 */
 function Sumar () 
 {
+	var precio1;
+	var precio2;
+	var precio3;
+	var suma;
+
+	precio1 = PrecioUno.value;
+	precio2 = PrecioDos.value;
+	precio3 = PrecioTres.value;
+
+	precio1 = parseInt(precio1); 
+	precio2 = parseInt(precio2);
+	precio3 = parseInt(precio3);
+
+	suma = precio1+precio2+precio3;
+
+	alert("Suma: "+suma);	
 	
 }
 function Promedio () 
 {
+	var precio1;
+	var precio2;
+	var precio3;
+	var promedio;
+
+	precio1 = PrecioUno.value;
+	precio2 = PrecioDos.value;
+	precio3 = PrecioTres.value;
+
+	precio1 = parseInt(precio1); 
+	precio2 = parseInt(precio2);
+	precio3 = parseInt(precio3);
+
+	promedio = (precio1+precio2+precio3)/3;
+	
+	alert("Promedio: "+promedio);	
 	
 }
 function PrecioFinal () 
 {
+	var precio1;
+	var precio2;
+	var precio3;
+	var suma;
+	var precioFinal;
+
+	precio1 = PrecioUno.value;
+	precio2 = PrecioDos.value;
+	precio3 = PrecioTres.value;
+
+	precio1 = parseInt(precio1); 
+	precio2 = parseInt(precio2);
+	precio3 = parseInt(precio3);
+
+	suma = precio1+precio2+precio3;
+	precioFinal = suma * 21/100 + suma;
+	precioFinal = precioFinal.toFixed(2)
+	
+	alert("Precio Final + IVA: "+precioFinal);	
 	
 }
