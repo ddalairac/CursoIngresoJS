44e86a7800d7c508d8022d5b71dc7267687b88d0
ddalairac <diegodalairac@gmail.com>
2019-02-14 19:42:09 -0300

    ejercicio2

--- 9-Parciales/2018_1Cuatri_Recu_fuentes/dos.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/dos.js
@@ -1,11 +1,20 @@
 function mostrar()
 {
-  
-	var nombre;
-	var localidad;
+  	var compra;
+  	var descuento;
+  	var iva
+  	var montoAdescontar;
+  	var precioConDescuento;
+  	var precioMasIva;
 
-	nombre = elNombre.value;
-	localidad = laLocalidad.value;
+	compra = prompt("Ingrese el valor de la compra");
+	compra=parseInt(compra);
+	iva = 21;
+  	descuento = 10;
+	montoAdescontar = compra * descuento/100;
+	precioConDescuento = compra - montoAdescontar;
+	precioMasIva = precioConDescuento * iva/100 + precioConDescuento;
 
-	alert("Usted es "+nombre+" y vive en la localidad "+localidad);
+	alert("Tu compra es de $"+compra+" tenes un descuento del 10% queda en $"+precioConDescuento+", mas iva es $"+precioMasIva+"")
+
 }
