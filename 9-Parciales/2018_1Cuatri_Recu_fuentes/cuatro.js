7411dd65da2e00bce4775eddf259b574549540b1
ddalairac <diegodalairac@gmail.com>
2019-02-14 20:31:36 -0300

    ejercicio4 + simple

--- 9-Parciales/2018_1Cuatri_Recu_fuentes/cuatro.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/cuatro.js
@@ -1,11 +1,13 @@
 function mostrar()
 {
 
-	var amigos = new Array;
-	var noHayMasAmigos;
+	//var amigos = new Array;
+	//var noHayMasAmigos;
 	var propina;
 	var iva;
 	var totalSinAdicionales;
+	var cantidadAmigos;
+	var montoPorAmigo;
 	var montoIva;
 	var montoPropina;
 	var total
@@ -13,32 +15,43 @@ function mostrar()
 	propina = 10;
 	iva = 21;
 	noHayMasAmigos = false;
-	//amigos = [5,2,9];
-	while(!noHayMasAmigos){
-		var nuevaComida
-		nuevaComida = prompt("Ingrese el valor de su comida");
-		nuevaComida=parseInt(nuevaComida);
 
-		if(isNaN(nuevaComida)){
-			alert("Ingrese un monto Valido");
-		} else {
-			amigos.push(nuevaComida);
-			noHayMasAmigos = confirm("Es el ultimo amigo?");
+	totalSinAdicionales = prompt("Ingrese el valor total de la comida");
+	totalSinAdicionales=parseInt(totalSinAdicionales);
+
+	cantidadAmigos = prompt("Ingrese la cantidad de amigos a dividir");
+	cantidadAmigos = parseInt(cantidadAmigos);
+
+	/*
+		//amigos = [5,2,9];
+		while(!noHayMasAmigos){
+			var nuevaComida
+			nuevaComida = prompt("Ingrese el valor de su comida");
+			nuevaComida=parseInt(nuevaComida);
+
+			if(isNaN(nuevaComida)){
+				alert("Ingrese un monto Valido");
+			} else {
+				amigos.push(nuevaComida);
+				noHayMasAmigos = confirm("Es el ultimo amigo?");
+			}
 		}
-	}
-	totalSinAdicionales = 0;
+		totalSinAdicionales = 0;
 
-	for(i=0;i<amigos.length;i++){
-		totalSinAdicionales += amigos[i];
-	}
+		for(i=0;i<amigos.length;i++){
+			totalSinAdicionales += amigos[i];
+		}
+	*/
 
 	montoPropina = totalSinAdicionales * propina/100;
 	montoIva = totalSinAdicionales * iva/100;
 	total = totalSinAdicionales + montoPropina + montoIva;
+	montoPorAmigo = total / cantidadAmigos;
 
 
-	alert("El total a pagar es de $"+total+". el total de los platos es de $"+totalSinAdicionales
-		+" mas $"+montoPropina+" de propina y $"+montoIva+" de iva");
+
+	alert("El total a pagar es de $"+total+". El monto por plato es de $"+montoPorAmigo
+		+". La propina es $"+montoPropina+" el iva es $"+montoIva+" de iva");
 
 }
 /*
