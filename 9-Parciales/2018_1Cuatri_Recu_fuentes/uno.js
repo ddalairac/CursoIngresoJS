214a2d0c21ef8d2410988cb3eef3edc0df1ea7e0
ddalairac <diegodalairac@gmail.com>
2019-02-14 19:20:37 -0300

    Ejercicio 1 y 2

--- 9-Parciales/2018_1Cuatri_Recu_fuentes/uno.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/uno.js
@@ -1,5 +1,20 @@
 
 function mostrar()
 {
+	var ancho;
+	var largo;
+	var perimetro
+	ancho = prompt("Ingrese el ancho del rectangulo");
+	largo = prompt("Ingrese el largo del rectangulo");
+	
+	ancho=parseInt(ancho);
+	largo=parseInt(largo);
+
+	perimetro = (ancho + largo) * 2;
+	alert("El perimetro del rectangulo: "+perimetro);
 
 }
+/*
+Realizar el algoritmo que pida el ancho y el largo de un rectángulo por prompt 
+y que muestre el perímetro por alert.
+*/
