567b4a4792c4ebaf8a3d20ac29049b53e3dbcf41
alumno <alumno@utnfra.com.ar>
2019-02-15 19:58:25 -0300

    adivina el numero

--- 8-TPs/jsAdivinaElNumero1.js
+++ 8-TPs/jsAdivinaElNumero1.js
@@ -9,18 +9,33 @@ de no ser igual se debe informar si “falta…”  para llegar al número secre
 */
 var numeroSecreto; 
 var contadorIntentos;
+ContadorDeIntentos = 0;
 
 function comenzar()
 {
-	//Genero el número RANDOM entre 1 y 100
-	 
-		//alert(numeroSecreto );
-	
-
+	numeroSecreto=(Math.floor(Math.random() *100) + 1);
+	//Genero el número RANDOM entre 1 y 100 
+	//alert(numeroSecreto );
 }
 
 function verificar()
 {
-	
-	
+	var numeroIngresado;
+	var mensaje;
+	ContadorDeIntentos = ContadorDeIntentos + 1;
+	intentos.value = ContadorDeIntentos;
+	numeroIngresado = numero.value
+	//alert("numeroIngresado "+numeroIngresado);
+	//alert("ContadorDeIntentos "+ContadorDeIntentos);
+
+	if(numeroIngresado == numeroSecreto){
+		mensaje = ("sos un genio, adivinaste en "+ContadorDeIntentos+" intentos");
+	} else {
+		if(numeroIngresado < numeroSecreto){
+			mensaje  = ("falta");
+		} else {
+			mensaje  = ("te pasaste");
+		}
+	}
+	alert(mensaje);
 }
