a6d3614ce8b87270922bf9be20778d0179f49a34
alumno <alumno>
2019-02-21 20:50:57 -0300

    switch

--- 9-Parciales/2018_1Cuatri_Recu_fuentes/cinco.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/cinco.js
@@ -1,4 +1,55 @@
-function mostrar()
-{
+function mostrar() {
+    var precioHabitacion;
+    var tipoPago;
+    var paquete;
+    var descuento;
+    var incremento;
+    var montoDescuento;
+    var montoAumento;
+    var total;
+
+    precioHabitacion = prompt("Ingrese el valor de la habitacion");
+    tipoPago = prompt("Ingrese tipo de pago");
+    precioHabitacion = parseInt(precioHabitacion);
+    descuento = 0;
+    incremento = 0;
+    montoDescuento = 0;
+    montoAumento = 0;
+
+    switch (tipoPago) {
+        case "visa":
+        case "mercadoPago":
+            descuento = 0.1
+            break
+        case "paypal":
+            descuento = 0.15
+            break
+        case "efectivo":
+            descuento = 0.20
+            paquete = prompt("Ingrese el codigo de paquete");
+            switch (paquete) {
+                case "soloDesayunos":
+                    incremento = 0.1
+                    break
+                case "todoIncluido":
+                    incremento = 0.15
+                    break
+            }
+            break
+        default:
+            descuento = 0.85
+    }
+    montoDescuento = precioHabitacion * descuento;
+    montoAumento = montoDescuento * incremento;
+
+    total = precioHabitacion - montoDescuento + montoAumento;
+
+    console.log("precioHabitacion: "+precioHabitacion);
+    console.log("descuento: "+descuento);
+    console.log("incremento: "+incremento);
+    console.log("montoDescuento: "+montoDescuento);
+    console.log("montoAumento: "+montoAumento);
+
+    alert("Total:"+total);
 
 }
--- 9-Parciales/2018_1Cuatri_Recu_fuentes/cuatro.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/cuatro.js
@@ -1,9 +1,72 @@
 function mostrar()
-{
+/* 
+recibir 4 precios
+avisar cual es el importe mas alto
+si el total es mayor a 100, hacer descuento de 10%
+si es mayor a 50, 5% descuento
+si es inferior a 50, PAGA 15% extra
+ */ {
 
-	//var amigos = new Array;
-	//var noHayMasAmigos;
-	var propina;
+	var precio1;
+	var precio2;
+	var precio3;
+	var precio4;
+	var suma;
+	var precioMasAlto;
+	var descuento;
+	var total;
+	var totalDescuento;
+	var mensaje;
+
+	precio1 = prompt("Ingrese el valor total de la comida");
+	precio2 = prompt("Ingrese el valor total de la comida");
+	precio3 = prompt("Ingrese el valor total de la comida");
+	precio4 = prompt("Ingrese el valor total de la comida");
+
+	precio1 = parseInt(precio1);
+	precio2 = parseInt(precio2);
+	precio3 = parseInt(precio3);
+	precio4 = parseInt(precio4);
+
+	totalDescuento = 0
+	suma = precio1 + precio2 + precio3 + precio4;
+
+	if (precio1 >= precio2 && precio1 >= precio3 && precio1 >= precio4) {
+		precioMasAlto = precio1;
+	} else {
+		if (precio2 >= precio3 && precio2 >= precio4) {
+			precioMasAlto = precio2;
+		} else {
+			if (precio3 >= precio4) {
+				precioMasAlto = precio3;
+			} else {
+				precioMasAlto = precio4;
+			}
+		}
+	}
+	mensaje = "El precio mas alto es: " + precioMasAlto;
+	alert(mensaje);
+
+
+	if (suma > 100) {
+		descuento = 10;
+	} else {
+		if (suma > 50) {
+			descuento = 5;
+		} else {
+			descuento = -15;
+		}
+	}
+
+	totalDescuento = suma * descuento / 100;
+	total = suma - totalDescuento;
+	mensaje = "total: " + total;
+
+	alert(mensaje);
+}
+
+/* 
+var propina;
 	var iva;
 	var totalSinAdicionales;
 	var cantidadAmigos;
@@ -22,7 +85,23 @@ function mostrar()
 	cantidadAmigos = prompt("Ingrese la cantidad de amigos a dividir");
 	cantidadAmigos = parseInt(cantidadAmigos);
 
-	/*
+	
+
+
+
+
+	montoPropina = totalSinAdicionales * propina/100;
+	montoIva = totalSinAdicionales * iva/100;
+	total = totalSinAdicionales + montoPropina + montoIva;
+	montoPorAmigo = total / cantidadAmigos;
+
+
+
+	alert("El total a pagar es de $"+total+". El monto por plato es de $"+montoPorAmigo
+		+". La propina es $"+montoPropina+" el iva es $"+montoIva+" de iva");
+ */
+
+/*
 		//amigos = [5,2,9];
 		while(!noHayMasAmigos){
 			var nuevaComida
@@ -42,21 +121,3 @@ function mostrar()
 			totalSinAdicionales += amigos[i];
 		}
 	*/
-
-	montoPropina = totalSinAdicionales * propina/100;
-	montoIva = totalSinAdicionales * iva/100;
-	total = totalSinAdicionales + montoPropina + montoIva;
-	montoPorAmigo = total / cantidadAmigos;
-
-
-
-	alert("El total a pagar es de $"+total+". El monto por plato es de $"+montoPorAmigo
-		+". La propina es $"+montoPropina+" el iva es $"+montoIva+" de iva");
-
-}
-/*
-Cuanto paga cada amigo
-total
-agregar 10% propipa
-agregar iva
-*/
