90f5546f35550e94d53263b1275350c42fffb732
diego dalairac <stereotipoarg@gmail.com>
2019-02-23 20:34:03 -0300

    TP3 (temperatura)

--- 8-TPs/jsFerretePinturas.js
+++ 8-TPs/jsFerretePinturas.js
@@ -2,12 +2,28 @@
 A.	Al ingresar una temperatura en Fahrenheit debemos mostrar la temperatura en Centígrados con un mensaje concatenado (ej.: " 32 Fahrenheit son 0 centígrados").
 B.	Al ingresar una temperatura en Centígrados debemos mostrar la temperatura en Fahrenheit (ej.: "0  centígrados son 32 Fahrenheit ").
 */
-function FahrenheitCentigrados () 
-{
-	
+
+var temperatura;
+var conversion;
+function FahrenheitCentigrados() {
+    temperatura = document.getElementById("Temperatura").value
+    temperatura = parseInt(temperatura);
+    conversion = temperatura - 32 * 5 / 9;
+    conversion = conversion.toFixed(0);
+    alert(temperatura + " °F = " + conversion + " °C ");
+
+
 }
 
-function CentigradosFahrenheit () 
-{
-	
+function CentigradosFahrenheit() {
+    temperatura = document.getElementById("Temperatura").value
+    temperatura = parseInt(temperatura);
+    conversion = temperatura * 5 / 9 + 32;
+    conversion = conversion.toFixed(0);
+    alert(temperatura + " °C = " + conversion + " °F");
+
 }
+/*
+°C × 9/5 + 32 = °F
+°F − 32 × 5/9 = °C
+ */
