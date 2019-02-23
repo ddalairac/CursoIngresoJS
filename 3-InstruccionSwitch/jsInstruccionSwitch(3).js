8b5936cb57f5c13c8139b1d7526292220fda81b7
diego dalairac <stereotipoarg@gmail.com>
2019-02-23 19:54:06 -0300

    ultimos switch

--- 3-InstruccionSwitch/jsInstruccionSwitch(3).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(3).js
@@ -15,3 +15,7 @@ function mostrar() {
 
 
 }//FIN DE LA FUNCIÓN
+
+/* 
+si es Febrero: " Este mes no tiene más de 29 días." 
+si NO es Febrero: "Este mes tiene 30 o más días" */
--- 3-InstruccionSwitch/jsInstruccionSwitch(5).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(5).js
@@ -1,12 +1,19 @@
-function mostrar()
-{
-//tomo la edad  
-var laHora = document.getElementById('hora').value;
-
-//alert (laHora);
-	
-	
-
-
-
+function mostrar() {
+    //tomo la edad  
+    var laHora;
+    laHora = document.getElementById('hora').value;
+    laHora = parseInt(laHora);
+    switch (laHora) {
+        case 7:
+        case 8:
+        case 9:
+        case 10:
+        case 11:
+            alert("es de mañana");
+            break;
+    }
 }//FIN DE LA FUNCIÓN
+
+/*
+Al ingresar una hora, informar:
+si está entre las 7 y las 11 : "Es de mañana.". */
